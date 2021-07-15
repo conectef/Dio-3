0f6107f30487fc46553d57af6e081cb73955bf71
Flavio <conectef@gmail.com>
2021-07-15 00:34:11 -0300

    1

---
+++ main.cs
@@ -0,0 +1,52 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System; 
+
+class minhaClasse {
+
+    static void Main(string[] args) { 
+
+            int t = Convert.ToInt32(Console.ReadLine());
+            double[] arrayList = new double[4];
+            int pa, pb;
+            double cpa, cpb;
+            int anos;
+
+            for (int i = 0; i < t; i++)
+            {
+                anos = 0;
+                string[] valores = Console.ReadLine().Split();
+                pa = int.Parse(valores[0]);
+                pb = int.Parse(valores[1]);
+
+               //declare as variaveis corretamente
+                cpa = 1000;
+                cpb = 2000;
+
+                while (pa <= pb)
+                {
+
+                   (1 ≤ T ≤ 3000)   //complete o while
+
+                    if (anos > 100)
+                    {
+                      ("Mais de 1 seculo.") //complete a condicional
+                    }
+                }
+
+                if (anos <= 100)
+                {
+                  (100)  //complete a condicional
+                }
+
+            }
+
+    }
+
+}
